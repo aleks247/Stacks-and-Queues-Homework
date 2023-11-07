8f0e68a54cb20f24af3de801b4b07cad08924099
Aleksandur Dimitrov <52452769+aleks247@users.noreply.github.com>
2023-11-08 00:01:44 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,7 +3,7 @@ from collections import deque
 food_quantity = int(input())
 orders = deque(map(int, input().split()))
 
-max_order = max(orders)
+biggest_order = max(orders)
 
 while orders:
     current_order = orders.popleft()
@@ -15,9 +15,10 @@ while orders:
         break
 
 if not orders:
-    print(max_order)
+    print(biggest_order)
     print("Orders complete")
 else:
     remaining_orders = ', '.join(map(str, orders))
-    print(max_order)
+    
+    print(biggest_order)
     print(f"Orders left: {remaining_orders}")
