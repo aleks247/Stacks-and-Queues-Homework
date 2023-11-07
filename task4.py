235d1441a32362acf7f827e228740e8da36a3450
Aleksandur Dimitrov <52452769+aleks247@users.noreply.github.com>
2023-11-07 23:59:41 +0200

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,22 +1,23 @@
 pallet = list(map(int, input().split()))
 shelf_capacity = int(input())
 
-shelves_used = 0
-current_shelf_capacity = 0
 stack = []
+
+used_shelves = 0
+current_shelf_capacity = 0
 
 for item in pallet:
     if current_shelf_capacity + item <= shelf_capacity:
         current_shelf_capacity += item
         stack.append(item)
     else:
-        shelves_used += 1
+        used_shelves += 1
         while stack:
             current_shelf_capacity -= stack.pop()
         current_shelf_capacity += item
         stack.append(item)
 
 if current_shelf_capacity > 0:
-    shelves_used += 1
+    used_shelves += 1
 
-print(shelves_used)
+print(used_shelves)
